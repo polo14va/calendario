9f11fec383ae9e7c22a8c701fe26ea596c328496
polo14va <polo14va@users.noreply.github.com>
2022-02-13 20:57:54 +0100

    Initial commit

---
+++ backgroundEvents.js
@@ -0,0 +1,11 @@
+var calendar = new Calendar(calendarEl, {
+  initialDate: "2014-11-10",
+  initialView: "timeGridWeek",
+  events: [
+    {
+      start: "2014-11-10T10:00:00",
+      end: "2014-11-10T16:00:00",
+      display: "background"
+    }
+  ]
+});
---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+// import timeGridPlugin from "@fullcalendar/timegrid";
+
+import "@fullcalendar/core/main.css";
+import "@fullcalendar/daygrid/main.css";
+import "@fullcalendar/timegrid/main.css";
+import events from "./events";
+export default function App() {
+  let firstDaty = 1;
+
+  return (
+    <div className="App">
+      <FullCalendar
+        defaultView="dayGridMonth"
+        firstDay={firstDaty}
+        locale="es"
+        header={{
+          left: "prev,next",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay"
+        }}
+        themeSystem="Simplex"
+        plugins={[dayGridPlugin]}
+        events={events}
+      />
+    </div>
+  );
+}
